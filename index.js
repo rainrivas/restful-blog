1f2763e4da2c1c5e45441033e7c1602cdbbfe56f
Rain <rainrivas@gmail.com>
2017-03-17 13:42:27 -0600

    Added EDIT and UPDATE routes to index. Also added method-override package to the app so we can perform a PUT action on the form. Sorted the list of the blogs on the index page to have the newest blog on top.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express'),
     app = express(),
     mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/restful_blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); // tell the app to override the method with desired params (we're using for PUT/DELETE)
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -23,7 +25,7 @@ app.get('/', function(req, res) {
 
 // INDEX ROUTE
 app.get('/blogs', function(req, res) {
-    Blog.find({}, function(err, blogs) {
+    Blog.find({}).sort({ "created": -1 }).exec(function(err, blogs) {
         if (err) {
             console.log("error: " + err);
         } else {
@@ -57,6 +59,30 @@ app.get('/blogs/:id', function(req, res) {
             res.redirect("/blogs");
         } else {
             res.render("show", { blog: foundBlog });
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            console.log(err);
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", { blog: foundBlog });
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put('/blogs/:id', function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if (err) {
+            console.log('error: ' + err);
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id);
         }
     });
 });
